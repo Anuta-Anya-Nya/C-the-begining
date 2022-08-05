763dd87379317883020dbca3df2864f88ab93c82
Aeshe <syslichek@mail.ru>
2022-08-06 00:27:29 +0300

    add task08

---
+++ S1-task08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2; //Смысла начинать счетчик с 1 нет, т.к. 1 в любом случае не надо выводить
+if (number <= 1) 
+{ 
+    Console.WriteLine("Введено неверное число"); 
+}
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
