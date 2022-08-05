acbf538a84e0ea137b6f23a84b2c6cec3d3af607
Aeshe <syslichek@mail.ru>
2022-08-05 23:52:13 +0300

    add task02

---
+++ S1-task02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.WriteLine("Числа равны!");
+}
+else if (num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+    Console.WriteLine("min = " + num2);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+    Console.WriteLine("min = " + num1);
+}
