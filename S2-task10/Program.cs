25cd887698ba559b3c5174370898745da0dec90f
Aeshe <syslichek@mail.ru>
2022-08-08 23:41:47 +0300

    Add task10 from homework2

---
+++ S2-task10/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number<100 || number>999) // ПРоверка на трехзначность
+{
+    Console.Write("Введенное число не трехзначное! Введите трехзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int remainder = number % 100; // остаток равен последним 2 цифрам
+int result = remainder / 10;
+Console.WriteLine($"Вторая цифра числа {number} равна {result}");
