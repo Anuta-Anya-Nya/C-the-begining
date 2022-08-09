6e9be292a534a74e074c61638b223a61525dc82e
Aeshe <syslichek@mail.ru>
2022-08-09 11:15:26 +0300

    Update task10 to Seminar-2

--- Seminar-2/S2-Task10/Program.cs
+++ Seminar-2/S2-Task10/Program.cs
@@ -2,6 +2,8 @@
 456 -> 5
 782 -> 8
 918 -> 1 */
+
+/* Решение 1
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 while (number<100 || number>999) // ПРоверка на трехзначность
@@ -12,4 +14,17 @@ while (number<100 || number>999) // ПРоверка на трехзначнос
 
 int remainder = number % 100; // остаток равен последним 2 цифрам
 int result = remainder / 10;
-Console.WriteLine($"Вторая цифра числа {number} равна {result}");
+Console.WriteLine($"Вторая цифра числа {number} равна {result}");*/
+
+//Решение 2:
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+while (number.Length != 3 || !int.TryParse(number, out int result))
+{
+    Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
+    number = Console.ReadLine();
+}
+int secondNumber = int.Parse(number[1].ToString());
+
+Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
