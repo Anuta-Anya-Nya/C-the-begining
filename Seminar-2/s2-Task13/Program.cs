c294e0275ea13960e60beb089bdf4f72323d23a3
Aeshe <syslichek@mail.ru>
2022-08-10 15:12:12 +0300

    Update task13 - add methods

--- Seminar-2/s2-Task13/Program.cs
+++ Seminar-2/s2-Task13/Program.cs
@@ -1,52 +1,60 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-/* Console.Write("Введите число: ");
-string number = Console.ReadLine();
-
-while (!int.TryParse(number, out int _)) //Проверка что введено число, а не текст
+// в методе 2 подумать и переделать ветвление, используя модуль
+void FindThirdNumber2(string numStr)
 {
-    Console.Write("Введенные данные текст! Введите число: ");
-    number = Console.ReadLine();
-}
-if (int.Parse(number) >= 0) // если число положительное
-{
-    if (number.Length <= 2)
+    while (!int.TryParse(numStr, out int _)) //Проверка что введено число, а не текст
     {
-        Console.WriteLine($"Третья цифра в числе {number} отсутствует");
+        Console.Write("Введенные данные текст! Введите число: ");
+        numStr = Console.ReadLine();
+    }
+    if (int.Parse(numStr) >= 0) // если число положительное
+    {
+        if (numStr.Length <= 2)
+        {
+            Console.WriteLine($"Третья цифра в числе {numStr} отсутствует");
+        }
+        else
+        {
+            int thirdNumber = int.Parse(numStr[2].ToString());
+            Console.WriteLine($"Третья цифра числа {numStr} равна {thirdNumber}");
+        }
     }
     else
     {
-        int thirdNumber = int.Parse(number[2].ToString());
-        Console.WriteLine($"Третья цифра числа {number} равна {thirdNumber}");
+        if (numStr.Length <= 3) //если число отрицательное
+        {
+            Console.WriteLine($"Третья цифра в числе {numStr} отсутствует");
+        }
+        else
+        {
+            int thirdNumber = int.Parse(numStr[3].ToString());
+            Console.WriteLine($"Третья цифра числа {numStr} равна {thirdNumber}");
+        }
     }
 }
-else
+void FindThirdNumber(string numberStr)
 {
-    if (number.Length <= 3) //если число отрицательное
+    while (!int.TryParse(numberStr, out int number)) //Проверка что введено число, а не текст
     {
-        Console.WriteLine($"Третья цифра в числе {number} отсутствует");
+        Console.Write("Введенные данные - это текст! Введите число: ");
+        numberStr = Console.ReadLine();
+    }
+    int numberPositive = Math.Abs(Convert.ToInt32(numberStr));
+
+    while (numberPositive > 999) //Делаем трехзначное число, откидывая последние цифры
+    {
+        numberPositive = numberPositive / 10;
+    }
+    if (numberPositive > 99 && numberPositive < 1000)  // если число трехзначное (положительное и отрицательное)
+    {
+        Console.WriteLine($"Третья цифра числа {numberStr} равна {numberPositive % 10}"); //Math.Abs(x) модуль
     }
     else
     {
-        int thirdNumber = int.Parse(number[3].ToString());
-        Console.WriteLine($"Третья цифра числа {number} равна {thirdNumber}");
+        Console.WriteLine($"Третья цифра числа отсутствует");
     }
-} */
-
+}
 Console.Write("Введите число: ");
-if (!int.TryParse(Console.ReadLine(), out int number)) //Проверка что введено число, а не текст
-{
-    Console.Write("Введенные данные - это текст!");
-    return;
-}
-while (number > 999 || number < -999) //Делаем трехзначное число, откидывая последние цифры
-{
-    number = number / 10;
-}
-if ((number > 99 && number < 1000) || (number > -999 && number < -99)) // если число трехзначное (положительное и отрицательное)
-{
-   Console.WriteLine($"Третья цифра числа равна {Math.Abs(number % 10)}"); //Math.Abs(x) модуль
-}
-else 
-{
-    Console.WriteLine($"Третья цифра числа отсутствует");
-}
+string num = Console.ReadLine();
+FindThirdNumber(num);
+FindThirdNumber2(num);
