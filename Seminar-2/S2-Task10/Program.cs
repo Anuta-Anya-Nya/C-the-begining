4906568959da4a173f6845e3ed522515ae00cd4b
Aeshe <syslichek@mail.ru>
2022-08-10 14:32:59 +0300

    Update task10 - add methods

--- Seminar-2/S2-Task10/Program.cs
+++ Seminar-2/S2-Task10/Program.cs
@@ -3,40 +3,38 @@
 782 -> 8
 918 -> 1 */
 
+void FindSecondNumber(string s)
+{
+    while (!int.TryParse(s, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
+    {
+        Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
+        s = Console.ReadLine();
+    }
+    int number = Convert.ToInt32(s);
+    int secondNumber = Math.Abs(number) % 100 / 10;
+    Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
+}
+//Решение 2: 
+void FindSecondNumber2( string str)
+{
+    while (!int.TryParse(str, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
+    {
+        Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
+        str = Console.ReadLine();
+    }
+    int secondNumber = 0;
+    if (Convert.ToInt32(str) > 0)
+    {
+        secondNumber = int.Parse(str[1].ToString());
+    }
+    else
+    {
+        secondNumber = int.Parse(str[2].ToString());
+    }
+    Console.WriteLine($"Вторая цифра числа {str} равна {secondNumber}");
+}
+
 Console.Write("Введите трехзначное число: ");
-string numberStr = Console.ReadLine();
-
-while (!int.TryParse(numberStr, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
-{
-    Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
-    numberStr = Console.ReadLine();
-}
-int number = Convert.ToInt32(numberStr);
-int secondNumber = Math.Abs(number)%100/10;
-Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
-
-/* int number = Convert.ToInt32(Console.ReadLine());
-int numberPos = Math.Abs(number);
-while (numberPos < 100 || numberPos > 999 ) // ПРоверка на трехзначность
-{
-    Console.Write("Введенное число не трехзначное! Введите трехзначное число: ");
-    number = Convert.ToInt32(Console.ReadLine());
-    numberPos = Math.Abs(number);
-}
-
-result = numberPos % 100 / 10; // остаток равен последним 2 цифрам
-
-*/
-//Решение 2: number.Length != 4
-/* Console.Write("Введите трехзначное число: ");
 string number = Console.ReadLine();
-
-while (!int.TryParse(number, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
-{
-    Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
-    number = Console.ReadLine();
-}
-
-int secondNumber = int.Parse(number[1].ToString());
-
-Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}"); */
+FindSecondNumber(number);
+FindSecondNumber2(number);
