25bcfc61fa8459bd49e77026b17c433e9537b2bc
Aeshe <syslichek@mail.ru>
2022-08-10 12:13:22 +0300

    update Task10

--- Seminar-2/S2-Task10/Program.cs
+++ Seminar-2/S2-Task10/Program.cs
@@ -3,28 +3,40 @@
 782 -> 8
 918 -> 1 */
 
-/* Решение 1
 Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-while (number<100 || number>999) // ПРоверка на трехзначность
+string numberStr = Console.ReadLine();
+
+while (!int.TryParse(numberStr, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
+{
+    Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
+    numberStr = Console.ReadLine();
+}
+int number = Convert.ToInt32(numberStr);
+int secondNumber = Math.Abs(number)%100/10;
+Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
+
+/* int number = Convert.ToInt32(Console.ReadLine());
+int numberPos = Math.Abs(number);
+while (numberPos < 100 || numberPos > 999 ) // ПРоверка на трехзначность
 {
     Console.Write("Введенное число не трехзначное! Введите трехзначное число: ");
     number = Convert.ToInt32(Console.ReadLine());
+    numberPos = Math.Abs(number);
 }
 
-int remainder = number % 100; // остаток равен последним 2 цифрам
-int result = remainder / 10;
-Console.WriteLine($"Вторая цифра числа {number} равна {result}");*/
+result = numberPos % 100 / 10; // остаток равен последним 2 цифрам
 
-//Решение 2:
-Console.Write("Введите трехзначное число: ");
+*/
+//Решение 2: number.Length != 4
+/* Console.Write("Введите трехзначное число: ");
 string number = Console.ReadLine();
 
-while (number.Length != 3 || !int.TryParse(number, out int result))
+while (!int.TryParse(number, out int result) || Math.Abs(result) < 100 || Math.Abs(result) > 999)
 {
     Console.Write("Введенные данные текст или не трехзначное число! Введите трехзначное число: ");
     number = Console.ReadLine();
 }
+
 int secondNumber = int.Parse(number[1].ToString());
 
-Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
+Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}"); */
