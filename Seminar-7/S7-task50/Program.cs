cc829e55c5f3c8cd11e51791e9bc8e9e099fd0c8
Aeshe <syslichek@mail.ru>
2022-08-26 00:19:11 +0300

    Домашка к семинару 7

---
+++ Seminar-7/S7-task50/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+void FindItem(int[,] matr, int position1, int position2)
+{
+    if (position1 > matr.GetLength(0) || position1 < 1
+            || position2 > matr.GetLength(1) || position2 < 1)
+    {
+        Console.WriteLine("Элемента с заданной позицией не существует");
+    }
+    else
+    {
+        Console.WriteLine($"Элемент с заданной позицией {position1} и {position2} имеет индекс [{position1 - 1}, {position2 - 1}] и равен {matr[position1 - 1, position2 - 1]}.");
+    }
+}
+
+Console.Write("Введите число m - количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n - количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента по горизонтали в массиве: ");
+int horiz = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента по вертикали в массиве: ");
+int vert = Convert.ToInt32(Console.ReadLine());
+FindItem(CreateMatrix(m, n), horiz, vert);
---
+++ Seminar-7/S7-task52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void CountAverage(int[,] matr)
+{
+    double average = 0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            average += matr[j, i];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {i + 1} равно {average / matr.GetLength(0)}.");
+        average = 0;
+    }
+}
+
+Console.Write("Введите число m - количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n - количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+CountAverage(CreateMatrix(m, n));
+
