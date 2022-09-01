3c74219910ee02b4087bc0912138637b04bbaff2
Aeshe <syslichek@mail.ru>
2022-09-02 00:29:35 +0300

    Mod some tasks

--- Seminar-5/S5-Task35/Program.cs
+++ Seminar-5/S5-Task35/Program.cs
@@ -29,3 +29,19 @@ for (int i = 0; i < arr.Length; i++)
     }
 }
 Console.WriteLine(count);
+
+// int[] array = new int[100];
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 1000);
+//     Console.Write(array[i] + ",");
+// }
+// Console.WriteLine();
+// for (int j = 0; j < array.Length; j++)
+// {
+//     if (array[j] > 9 & array[j] < 100) count = count + 1;
+// }
+// Console.WriteLine();
+// Console.Write($"на отрезке [10,99] содержится - {count} элементов");
