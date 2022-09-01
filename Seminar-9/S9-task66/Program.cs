7accf8570397ef6578d520bab0ab35b0505414ac
Aeshe <syslichek@mail.ru>
2022-09-02 00:23:07 +0300

    Домашка к семинару 9

---
+++ Seminar-9/S9-task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int M = 4;
+int N = 8;
+int SumElements(int number1, int number2)
+{
+    // int sum = 0;
+    if (number1 > number2) return 0;
+    return number1 + SumElements(number1 + 1, number2);
+}
+
+if (M < N)
+{
+    Console.WriteLine(SumElements(M, N));
+}
+else
+{
+    Console.WriteLine(SumElements(N, M));
+}
---
+++ Seminar-9/S9-task68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+int FuncAkkerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (n > 0 && m == 0)
+    {
+        return FuncAkkerman(n - 1, 1);
+    }
+    else
+    {
+        return FuncAkkerman(n - 1, FuncAkkerman(n, m - 1));
+    }
+
+}
+int m = 2, n = 3;
+Console.WriteLine(FuncAkkerman(n, m));
